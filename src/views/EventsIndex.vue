de21e862f5c48d84391e26d425042da9cc28e75b
greg <gregorybwest@gmail.com>
2022-03-08 23:14:00 -0800

    adds EventsIndex.vue, gets 404

---
+++ src/views/EventsIndex.vue
@@ -0,0 +1,29 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      events: [],
+    };
+  },
+  created: function () {
+    axios.get("/events").then((response) => {
+      console.log("events index:", response);
+      this.events = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <div class="events-index">
+    <h1>All Events</h1>
+    <div v-for="event in events" v-bind:key="event.id">
+      <h2>{{ event.yelp_venue }}</h2>
+    </div>
+  </div>
+</template>
+
+<style></style>
