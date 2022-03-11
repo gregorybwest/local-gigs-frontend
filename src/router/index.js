7d0219a6e9e8f99101cfa30ae0ce133f315ad538
greg <gregorybwest@gmail.com>
2022-03-11 14:22:03 -0800

    import UsersShow and path to /users/:id

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Login from "../views/Login.vue";
 import Logout from "../views/Logout.vue";
 import EventsIndex from "../views/EventsIndex.vue";
 import EventsShow from "../views/EventsShow.vue";
+import UsersShow from "../views/UsersShow.vue";
 
 const routes = [
   { path: "/signup", name: "signup", component: Signup },
@@ -11,6 +12,7 @@ const routes = [
   { path: "/logout", name: "logout", component: Logout },
   { path: "/events", name: "EventsIndex", component: EventsIndex },
   { path: "/events/:id", name: "EventsShow", component: EventsShow },
+  { path: "/users/:id", name: "UsersShow", component: UsersShow },
 ];
 
 const router = createRouter({
