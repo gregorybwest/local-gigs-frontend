afd6ee28521f1cf69c54da87947596936da9ea7d
greg <gregorybwest@gmail.com>
2022-03-14 15:05:06 -0700

    adds show_time to EventsShow page

--- src/views/EventsShow.vue
+++ src/views/EventsShow.vue
@@ -13,7 +13,17 @@ export default {
       this.event = response.data;
     });
   },
-  methods: {},
+  methods: {
+    destroyEvent: function () {
+      if (confirm("Are you sure you want to delete this?")) {
+        axios.delete(`events/${this.event.id}`).then((response) => {
+          console.log("Event deleted", response.data);
+          localStorage.setItem("flashMessage", "Event successfully deleted");
+          this.$router.push("/events");
+        });
+      }
+    },
+  },
 };
 </script>
 
@@ -24,10 +34,11 @@ export default {
     </router-link>
     <h1>@ {{ this.event.yelp_venue.name }}</h1>
     <h2>{{ this.event.yelp_venue.location[0] }}, {{ this.event.yelp_venue.location[1] }}</h2>
+    <h3>Show Time: {{ event.show_time }}</h3>
     <img v-bind:src="this.event.flier_image_url" alt="" />
     <div v-if="event.owner">
-      <button><router-link v-bind:to="`/events/${event.id}/edit`">Edit</router-link></button>
-      <br />
+      <button><router-link v-bind:to="`/events/${event.id}/edit`">Edit Event</router-link></button>
+      |
       <button v-on:click="destroyEvent()">Delete Event</button>
     </div>
   </div>
