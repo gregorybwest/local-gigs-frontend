02aa097d767be2cd059aad9d37578dd7e8dcd11d
greg <gregorybwest@gmail.com>
2022-03-14 09:42:42 -0700

    import and path for EventsEdit and UsersEdit

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,8 @@ import EventsIndex from "../views/EventsIndex.vue";
 import EventsShow from "../views/EventsShow.vue";
 import UsersShow from "../views/UsersShow.vue";
 import EventsNew from "../views/EventsNew.vue";
+import EventsEdit from "../views/EventsEdit.vue";
+import UsersEdit from "../views/UsersEdit.vue";
 
 const routes = [
   { path: "/signup", name: "signup", component: Signup },
@@ -15,6 +17,8 @@ const routes = [
   { path: "/events/:id", name: "EventsShow", component: EventsShow },
   { path: "/users/:id", name: "UsersShow", component: UsersShow },
   { path: "/events/new", name: "EventsNew", component: EventsNew },
+  { path: "/events/:id/edit", name: "EventsEdit", component: EventsEdit },
+  { path: "/users/:id/edit", name: "UsersEdit", component: UsersEdit },
 ];
 
 const router = createRouter({
