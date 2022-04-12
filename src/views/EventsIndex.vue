5c8672afc184d8ce717627518a52bf1aae85bcfb
greg <gregorybwest@gmail.com>
2022-04-12 12:35:11 -0700

    adds geolocate control to the map

--- src/views/EventsIndex.vue
+++ src/views/EventsIndex.vue
@@ -29,6 +29,18 @@ export default {
           .addTo(map);
         console.log(marker1);
       });
+      // Add geolocate control to the map.
+      map.addControl(
+        new mapboxgl.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true,
+          },
+          // When active the map will receive updates to the device's location as it changes.
+          trackUserLocation: true,
+          // Draw an arrow next to the location dot to indicate which direction the device is heading.
+          showUserHeading: true,
+        })
+      );
     });
   },
   methods: {},
